380cfb18fdf7f64dff07dc629786eb5b8fc63d10
tuviasu <tuviasu@gmail.com>
2025-10-22 20:42:48 +0300

    stage 0 first part

--- Stage0/Program5614.cs
+++ Stage0/Program5614.cs
@@ -1,12 +1,19 @@
 ﻿namespace Stage0;
 
-public class Program
+public partial class Program
 {
     public static void Main(string[] args)
+    {
+        Welcome5614();
+        Welcome4320();
+        Console.ReadKey();
+    }
+
+    static partial void Welcome4320();
+    private static void Welcome5614()
     {
         Console.WriteLine("enter your user name");
         string userName = Console.ReadLine()!;
         Console.WriteLine($"{userName}, welcome to my first application");
-        Console.ReadKey();
     }
 }
