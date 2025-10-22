9d0d99404de59395982913ac66cabb20e28a5fc2
tuviasu <tuviasu@gmail.com>
2025-10-22 20:29:12 +0300

    initial commit stage 0

---
+++ Stage0/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Stage0;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("enter your user name");
+        string userName = Console.ReadLine()!;
+        Console.WriteLine($"{userName}, welcome to my first application");
+        Console.ReadKey();
+    }
+}
